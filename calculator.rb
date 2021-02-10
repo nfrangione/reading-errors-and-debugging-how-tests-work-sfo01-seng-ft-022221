8019350b60e37e97378e8fc5d9f7b4907e0fa83f
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-10 01:45:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
+first_number = 58
+second_number = 13 
 def calculator()
-  first_number = 58
-  second_number = 13 
   sum = first_number + second_number
   difference = first_number - second_number
   product = first_number * second_number
