5c79a14620b90004c9101be4b2eb49ceccf5c898
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-10 01:42:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,10 @@
 # Add your variables here
 def calculator()
-  first_number = 8
+  first_number = 58
+  second_number = 13 
+  sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
 end
 
